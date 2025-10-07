668686d6c744aff634fc7eb73262c31192573a80
Karina Verkholomova <karina.verkholomova@gmail.com>
2025-10-07 12:10:43 +0200

    style: change light theme color scheme

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  scaffoldBackgroundColor: const Color.fromRGBO(237,241,245, 1.0),
-  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-    type: BottomNavigationBarType.fixed,
-    backgroundColor: Color.fromRGBO(226,232,240, 1.0),
-    selectedItemColor: Color.fromRGBO(31,44,59, 1.0),
-    unselectedItemColor: Color.fromRGBO(62,88,117, 1.0),
+  scaffoldBackgroundColor: Colors.deepPurpleAccent[50],
+  appBarTheme: AppBarThemeData(
+    backgroundColor: Colors.deepPurpleAccent[50],
+    titleTextStyle: TextStyle(
+      color: Colors.deepPurpleAccent[400],
+    )
   ),
 );
