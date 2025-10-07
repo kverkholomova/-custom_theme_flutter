e3464172664b212ed0e5dcc4d9e5dd2118c7512b
Karina Verkholomova <karina.verkholomova@gmail.com>
2025-10-07 11:50:03 +0200

    feat: add project skeleton

---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+import 'package:test_project_2/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Custom Themes'),
+    );
+  }
+}
